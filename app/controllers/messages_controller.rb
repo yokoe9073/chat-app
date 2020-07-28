6b6edadad1f19f40090be6e0a82ae5a101f07d2d
yokoe9073 <yokoe9073@gmail.com>
2020-07-28 12:14:33 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
+
   def index
      @message = Message.new
      @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
    end
  
    def create
@@ -13,7 +15,6 @@ class MessagesController < ApplicationController
        render :index
      end
    end
- 
  
    private
  
